8caddc3200130c5157ff46f90bb9e64f5c24bf65
matt.sol <matthewcpfeifer@gmail.com>
2022-06-06 15:09:48 -0400

    navbar now integrated into index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,21 @@
-import Home from "./Home";
+import Home from "../components/Home";
+import NavBar from "../components/shared/Navbar";
 import Footer from "../components/Footer";
 
 const Landing = () => (
-    <div>
-			<div className="bg-light h-screen flex flex-col justify-between">
-				<div><Home /></div>
-				<div><Footer /></div>
-			</div>
+  <div>
+    <div className="bg-light h-screen flex flex-col justify-between">
+      <div>
+        <NavBar />
+      </div>
+      <div>
+        <Home />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </div>
+  </div>
 );
 
 export default Landing;
