d0a51513a8b65eae0afca3c7767a1e4271c9d6ee
matt.sol <matthewcpfeifer@gmail.com>
2022-06-06 15:09:22 -0400

    about added

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import NavBar from "../components/shared/Navbar";
+
+const About = () => (
+  <div>
+    <div>
+      <NavBar />
+    </div>
+    <div className="bg-light h-screen flex flex-col justify-between">
+      <div>This is about</div>
+    </div>
+  </div>
+);
+
+export default About;
