9c4cd6a02607cdc278b11cd0cfdf7b70428f8e0b
matt.sol <matthewcpfeifer@gmail.com>
2022-06-23 16:51:46 -0400

    created records page

---
+++ src/pages/records.js
@@ -0,0 +1,14 @@
+import NavBar from "../components/shared/Navbar";
+
+const Records = () => {
+  return (
+    <div>
+      <div>
+        <NavBar />
+      </div>
+      This is the collection!
+    </div>
+  );
+};
+
+export default Records;
