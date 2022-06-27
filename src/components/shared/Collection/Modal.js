c614b00c770e792f7c5e428f7f4af40e53125387
matt.sol <matthewcpfeifer@gmail.com>
2022-06-27 11:06:13 -0400

    music modal now a square no matter what

--- src/components/shared/Collection/Modal.js
+++ src/components/shared/Collection/Modal.js
@@ -3,13 +3,13 @@ import Close from "../Close";
 
 const Modal = ({ image, artist, title, mp3, handleMenu }) => {
   return (
-    <div className="rounded-xl bg-metacrateRed text-metacrateWhite px-6 py-8 mx-2 my-3 z-50">
+    <div className="rounded-xl bg-metacrateRed text-metacrateWhite px-12 py-8 mx-2 my-3 z-50">
       <div className="absolute top-8 right-8" onClick={() => {handleMenu()}}>
         <Close color="white" />
       </div>
       <p>{title}</p>
-      <Image src={image} width="250px" height="250px" alt="music NFT image" />
-      <p className=" font-extrabold">{artist}</p>
+      <Image src={image} width="179px" height="179px" alt="music NFT image" />
+      <p className="font-extrabold">{artist}</p>
     </div>
   ) 
 }
