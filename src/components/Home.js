d1fec8a707e8c2c0ea246cc16f7f7d7ff735bfa1
matt.sol <matthewcpfeifer@gmail.com>
2022-07-06 12:06:30 -0400

    macbook pro 14in view

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,12 @@ import YouTubeEmbed from "./YouTubeEmbed";
 
 const Home = () => {
   return (
-    <div className="h-full -mt-10 2xl:-mt-20 3xl:-mt-32 w-full text-center flex flex-col justify-between">
+    <div className="h-full -mt-10 xl:-mt-28 3xl:-mt-32 w-full text-center flex flex-col justify-between">
       <div className="text-[32px] md:text-[48px] uppercase">
         Welcome to <span className="text-metacrateRed">MetaCrate.</span>
       </div>
       <div className="hidden md:flex justify-center py-4">
-        <YouTubeEmbed embedId="A91ymdbTZlA" width={720} height={402} />
+        <YouTubeEmbed embedId="A91ymdbTZlA" width={600} height={320} />
       </div>
       <div className="md:hidden flex justify-center py-4">
         <YouTubeEmbed embedId="A91ymdbTZlA" width={300} height={200} />
